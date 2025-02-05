14f40274fdf6eaf68f491ec62463fc1ff103bb37
manamjong <nskynet5374@naver.com>
2025-02-05 17:49:02 +0900

    실습문제

---
+++ exam/q2-1.py
@@ -0,0 +1,5 @@
+num1 = 10
+num2 = 20
+result = num1 + num2
+
+print("두 수의 합 : %d" % result)
---
+++ exam/q2-3.py
@@ -0,0 +1,5 @@
+num1 = 10
+num2 = 20
+result = num1 + num2
+
+print(str(num1) + " + " + str(num2) + " = " + str(result))
