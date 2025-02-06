6b0ed9744165c3c57109c0dbaec0597183497cfd
manamjong <nskynet5374@naver.com>
2025-02-06 10:37:58 +0900

    실습문제(format활용)

---
+++ exam/q2-8.py
@@ -0,0 +1,11 @@
+h1 = int(input("윗변의 길이는?"))
+h2 = int(input("밑변의 길이는?"))
+h3 = int(input("높이는?"))
+
+result = (h1+h2) * h3 / 2
+
+
+asdf = "{:.1f}".format(result)
+
+print(asdf)
+print("-사다리꼴의 면적 : %.1f" % (result))
