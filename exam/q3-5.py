e2cddf1ea73af213a43b57a2c108c5c44d4d33d2
manamjong <nskynet5374@naver.com>
2025-02-06 14:06:34 +0900

    if문 기초실습

--- exam/q3-5.py
+++ exam/q3-5.py
@@ -2,7 +2,6 @@ num1 = int(input("첫 번째 정수 : "))
 num2 = int(input("두 번째 정수 : "))
 num3 = int(input("세 번째 정수 : "))
 
-numArr = [num1, num2, num3]
 
 
 if num1 > num2 and num1 > num3:
@@ -13,4 +12,5 @@ else:
     print("%d, %d, %d 중에서 가장 큰 수는 %d입니다." % (num1, num2, num3, num3))
 
 # max함수 사용
+# numArr = [num1, num2, num3]
 # print("%d, %d, %d 중에서 가장 큰 수는 %d입니다." % (num1, num2, num3, max(numArr)))
