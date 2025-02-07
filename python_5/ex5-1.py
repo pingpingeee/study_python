8a1ca6d00abc35b44df95fde96a631eadcd0d20b
manamjong <nskynet5374@naver.com>
2025-02-07 17:20:25 +0900

    문자열 replace()

---
+++ python_5/ex5-1.py
@@ -0,0 +1,7 @@
+list1 = [3, 15, -12.5, "사과", "딸기"]
+print(list1)
+
+# list2 = list(range(10)) # 0 ~ 9
+# list2 = list(range(1, 10)) # 1 ~ 9
+list2 = list(range(1, 21, 2))  # 1에서 21 출력인데 2씩 증가
+print(list2)
