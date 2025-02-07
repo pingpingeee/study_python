550eaef5e9007fb7166862d57acf1f0ebc9907c5
manamjong <nskynet5374@naver.com>
2025-02-07 17:43:56 +0900

    리스트 붙히기

---
+++ python_5/ex5-14.py
@@ -0,0 +1,6 @@
+person1 = ["kim", 24, "kim@naver.com"]
+person2 = ["lee", 35, "kim@naver.com"]
+
+person = person1 + person2
+
+print(person)  # ['kim', 24, 'kim@naver.com', 'lee', 35, 'kim@naver.com'] 출력
