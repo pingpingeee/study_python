e8f854f5b21aa4d8e9500361173fcc9107d2c800
manamjong <nskynet5374@naver.com>
2025-02-07 17:44:39 +0900

    리스트 sum()

---
+++ python_5/ex5-15.py
@@ -0,0 +1,8 @@
+scores = [80, 90, 85, 95, 100]
+
+# sum() => 리스트 안의 값을 다 더함
+sm = sum(scores)
+avg = sum(scores) / len(scores)
+
+print("합계 : ", sm) # 합계 :  450 출력
+print("평균 : ", avg) # 평균 :  90.0 출력
