5aa4c818e98b09c5869d97691b52f03800830adf
manamjong <nskynet5374@naver.com>
2025-02-07 17:42:31 +0900

    리스트 remove()

---
+++ python_5/ex5-11.py
@@ -0,0 +1,6 @@
+members = ["홍길동", 20, "경기도 김포시", "test@naver.com", "010-1234-1234"]
+print(members)
+
+# remove() => 해당 값 삭제
+members.remove(20)
+print(members)  # ['홍길동', '경기도 김포시', 'test@naver.com', '010-1234-1234'] 출력
