ca3da3e4aa390d57ce6e8abee79c4f83645023fc
manamjong <nskynet5374@naver.com>
2025-02-07 14:11:21 +0900

    while문 내에 if문

--- exam_4/q4-6.py
+++ exam_4/q4-6.py
@@ -1,11 +1,11 @@
 i = 0
-count = 1
+count = 0
 
 while i <= 1000:
     if i % 3 != 0:
         print(i, end=" ")
         count += 1
-    if count == 10:
+    if count == 0:
         count = 1
         print("")
     i += 1
