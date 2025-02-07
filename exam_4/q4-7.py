d2a6784a465bcc53f5dd8441869b1e49bcdc2288
manamjong <nskynet5374@naver.com>
2025-02-07 14:11:28 +0900

    for문 내에 if문

--- exam_4/q4-7.py
+++ exam_4/q4-7.py
@@ -1,9 +1,9 @@
-count = 1
+count = 0
 
 for i in range(200, 800):
     if i % 5 != 0:
         print(i, end=" ")
         count += 1
     if count == 10:
-        count = 1
+        count = 0
         print("")
