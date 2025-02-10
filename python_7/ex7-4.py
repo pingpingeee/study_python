f6a0dc6be8a1183c3ce9359834b77c7a4806b52a
manamjong <nskynet5374@naver.com>
2025-02-10 15:21:01 +0900

    함수 내 append()

---
+++ python_7/ex7-4.py
@@ -0,0 +1,15 @@
+def func(food):  # 매개변수를 리스트로 받아서 처리 가능
+    # for x in food:
+    # print(x)
+    food.append("딸기")
+    food.append("수박")
+
+
+# print(["사과", "오렌지", "바나나"])
+fruits = ["사과", "오렌지", "바나나"]
+
+# func(fruits)
+# print(fruits)
+
+func(fruits)
+print(fruits)  # 함수에서 append 반영되어서 출력
