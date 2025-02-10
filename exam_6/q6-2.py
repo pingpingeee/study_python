5453f563809cf15e17506b03c50777ebd0b0f63e
manamjong <nskynet5374@naver.com>
2025-02-10 12:44:47 +0900

    요일, 온도 전체 출력

---
+++ exam_6/q6-2.py
@@ -0,0 +1,21 @@
+temp = {
+    "월": 15.5,
+    "화": 17.0,
+    "수": 16.2,
+    "목": 12.9,
+    "금": 11.0,
+    "토": 10.5,
+    "일": 13.3,
+}
+
+print("-" * 50)
+
+for i in temp:
+    print(i, end="\t")
+print()
+print("-" * 50)
+
+for i in temp:
+    print(temp[i], end="\t")
+print()
+print("-" * 50)
