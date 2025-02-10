d4ab6f699fcdcc9bd00d49feda93ec167e6f5cf0
manamjong <nskynet5374@naver.com>
2025-02-10 10:33:18 +0900

    리스트 split()

--- exam_5/q5-6.py
+++ exam_5/q5-6.py
@@ -2,4 +2,11 @@ file_names = ["file1.py", "file2.txt", "file3.pptx", "file4.doc"]
 
 for i in range(len(file_names)):
     file1 = file_names[i].split(".")
-    print(file_names[i] + " => " + "파일명 : " + file1[0]+ "," +"확장자 : " + file1[1])
+    print(
+        file_names[i] + " => " + "파일명 : " + file1[0] + ", " + "확장자 : " + file1[1]
+    )
+# 출력
+# file1.py => 파일명 : file1, 확장자 : py
+# file2.txt => 파일명 : file2, 확장자 : txt
+# file3.pptx => 파일명 : file3, 확장자 : pptx
+# file4.doc => 파일명 : file4, 확장자 : doc
