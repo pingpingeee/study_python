9d21468a15da0b964584aaf52efffcf97146f169
manamjong <nskynet5374@naver.com>
2025-02-10 12:11:24 +0900

    튜플 기초

---
+++ python_6/ex6-2.py
@@ -0,0 +1,22 @@
+# tup1 = (10,20,30,40,50)
+# tup1 = (10,20)
+# tup2 = (30,40)
+# tup3 = tup1 + tup2
+
+# for i in range(len(tup1)):
+#     print(tup1[i])
+
+# print(tup3)
+
+admin_info = ("admin", "12345", "webmaster@naver.com")
+
+print("관리자 정보")
+print("아이디: " + admin_info[0])
+print("비밀번호: " + admin_info[1])
+print("이메일: " + admin_info[2])
+
+# 출력
+# 관리자 정보
+# 아이디: admin
+# 비밀번호: 12345
+# 이메일: webmaster@naver.com
