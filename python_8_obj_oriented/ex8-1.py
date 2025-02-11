7d9dac7061fc02f40604446eddda4660b9131c2e
manamjong <nskynet5374@naver.com>
2025-02-11 12:38:12 +0900

    클래스 속성, 메소드 생성

--- python_8_obj_oriented/ex8-1.py
+++ python_8_obj_oriented/ex8-1.py
@@ -2,10 +2,11 @@ class Person:
     name = "김정연"  # name => 속성
 
     def hello(self):  # hello 메소드 호출될 때 객체를 self 받음
-        print("hello")
+        print(Person.name + "님 안녕하세요")
 
 
 person1 = Person()  # 객체 생성
 person1.hello()
 
-person1.name
+Person.name = "황서영"
+person1.hello()
