9529db535cde75837896a5081788e4cf42f8a803
manamjong <nskynet5374@naver.com>
2025-02-11 12:50:22 +0900

    math

---
+++ python_9_module/ex9-3.py
@@ -0,0 +1,17 @@
+import math
+
+print("sin(2) : ", math.sin(2))
+print("cos(2) : ", math.cos(2))
+print("cos(2) : ", math.cos(2))
+print("tan(2) : ", math.tan(2))
+print("ceil(12.3) : ", math.ceil(12.3))  # 올림 13
+print("floor(12.3) : ", math.floor(12.3))  # 내림 12
+print("round(12.3) : ", round(12.3))  # 반올림 12
+
+print(math.fsum([1, 2, 3, 4, 5]))  # 리스트합계
+print(math.fsum([1.2, 2.5, 3.8, 4.2, 5.9]))  # 리스트 실수 합계
+print(math.fsum((10, 20, 30, 40, 50)))  # 리스트 튜플 합계
+
+print("log(12.3) : ", math.log(75.3))
+print("pow(5, 2) : ", math.pow(5, 3))  # 5의 3제곱 125
+print("sqrt(25) : ", math.sqrt(25))  # 25의 제곱근 5
