cd34aed58cd64705359ae1d2eda1b5b52e179cc0
manamjong <nskynet5374@naver.com>
2025-02-11 12:50:17 +0900

    import

---
+++ python_9_module/ex9-1.py
@@ -0,0 +1,4 @@
+from python_9_m import hi
+
+hi.greet1("홍길동")
+hi.greet2("홍길길")
---
+++ python_9_module/ex9-2.py
@@ -0,0 +1,9 @@
+# from python_9_m import calc
+# import calc as c
+from python_9_m import calc
+
+x = calc.add(10, 20)
+# x = c.add(10, 20)
+# x=add(10,20)
+
+print(x)
